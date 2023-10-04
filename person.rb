25780473d55aa68e23d6e358771386e425bfbe9c
Lawmsangi <ddarkphoenixx22@gmail.com>
2023-10-04 15:58:26 +0530

    Add associations

--- person.rb
+++ person.rb
@@ -5,7 +5,7 @@ require_relative 'capitalize_decorator'
 
 class Person < Nameable
   attr_reader :id
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
 
   def initialize(age, name: 'Unknown', parent_permission: true)
     super()
@@ -13,6 +13,11 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
+  end
+
+  def add_rental(rental)
+    @rentals << rental
   end
 
   def correct_name
@@ -29,10 +34,3 @@ class Person < Nameable
 
   private :of_age?
 end
-
-person = Person.new(22, name: 'maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
--- student.rb
+++ student.rb
@@ -1,12 +1,20 @@
-require './person'
+require_relative 'person'
 
 class Student < Person
-  def initialize(age, classroom, parent_permission: true, name: 'Unknown')
-    super(age, name, parent_permission)
+  attr_accessor :name
+  attr_reader :classroom
+
+  def initialize(age, classroom, name: 'Unknown')
+    super(age, name: name)
+    self.classroom = classroom
+  end
+
+  def classroom=(classroom)
     @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
-    '¯(ツ)/¯'
+    '¯\\(ツ)/¯'
   end
 end
